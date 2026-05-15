48a96f12718580c5348f841a30c367a956465b2a
Larix <greiner.roli@gmail.com>
2026-05-15 16:34:52 +0200

    sync

--- main.cpp
+++ main.cpp
@@ -7,17 +7,20 @@
 #include <iostream>
 #include <fstream>
 
+#include "list.h"
+
 using namespace genv;
 using namespace std;
 
 const int WIDTH = 1200;
 const int HEIGHT = 1000;
 
-void EventLoop(vector<Widget*> widgets);
 void ClearWindow();
 
 class App {
     vector<Widget*> widgets;
+    List* list_a;
+    List* list_b;
 
 public:
     void Test() {
@@ -26,8 +29,15 @@ public:
     void Test2() {
         cout << "Bobr" << endl;
     }
-    App() {
-        /*
+    void MoveItem(List* list_a, List* list_b) {
+        string item = list_b->GetValue();
+        if (!item.empty()) {
+            list_a->AddItem(item);
+            list_b->RemoveCurrent();
+        }
+    }
+    void AddItem()
+    App() {/*
         widgets.push_back(new Widget(
             {WIDTH / 2, 50},
             {500, 80},
@@ -66,7 +76,6 @@ public:
             {255, 255, 255},
             "Test"
         ));
-        */
 
         vector<string> text;
         fstream fin("angol.txt");
@@ -78,13 +87,35 @@ public:
         fin.close();
 
         widgets.push_back(new TextField(
-            {WIDTH / 2, HEIGHT / 2},
+            {WIDTH / 2, 500},
             {500, 300},
             {200, 200, 200},
             text,
             16,
             true
-        ));
+        )); */
+        vector<string> elemek = {"egy", "kettő", "hat"};
+        list_a = new List({100, 100}, {150, 30}, {255, 255, 255}, elemek);
+        widgets.push_back(list_a);
+
+        elemek.clear();
+        elemek = {"alma", "körte", "banán", "málna"};
+        list_b = new List({300, 100}, {150, 30}, {255, 255, 255}, elemek);
+        widgets.push_back(list_b);
+
+        widgets.push_back(new Button(
+            {100, 50},
+            {100, 50},
+            {255, 255, 255},
+            "jobbra",
+            [this]{MoveItem(list_b, list_a);}));
+
+        widgets.push_back(new Button(
+            {300, 50},
+            {100, 50},
+            {255, 255, 255},
+            "balre",
+            [this]{MoveItem(list_a, list_b);}));
 
         ClearWindow();
         for (Widget* widget : widgets) {
@@ -92,8 +123,50 @@ public:
         }
         gout << refresh;
     }
+    void EventLoop() {
+        event ev;
+        int focus = -1;
+        while (gin >> ev && ev.keycode != key_escape) {
+            ClearWindow();
+            if (ev.button == btn_left) {
+                for (int i = 0; i < widgets.size(); i++) {
+                    if (widgets[i]->UnderMouse({ev.pos_x, ev.pos_y})) {
+                        focus = i;
+                        break;;
+                    }
+                    if (i == widgets.size() - 1) {
+                        focus = -1;
+                    }
+                }
+            }
+
+            if (focus != -1) {
+                widgets[focus]->Interact(ev);
+            }
+
+            if (ev.keycode == key_left) {
+                string item = list_b->GetValue();
+                if (!item.empty()) {
+                    list_a->AddItem(item);
+                    list_b->RemoveCurrent();
+                }
+            }
+            if (ev.keycode == key_right) {
+                string item = list_a->GetValue();
+                if (!item.empty()) {
+                    list_b->AddItem(item);
+                    list_a->RemoveCurrent();
+                }
+            }
+
+            for (Widget* widget : widgets) {
+                widget->Draw();
+            }
+            gout << refresh;
+        }
+    }
     void Start() {
-        EventLoop(widgets);
+        EventLoop();
         for (Widget* widget : widgets) {
             delete widget;
         }
@@ -109,34 +182,6 @@ int main() {
     app.Start();
 
     return 0;
-}
-
-void EventLoop(vector<Widget*> widgets) {
-    event ev;
-    int focus = -1;
-    while (gin >> ev && ev.keycode != key_escape) {
-        ClearWindow();
-        if (ev.button == btn_left) {
-            for (int i = 0; i < widgets.size(); i++) {
-                if (widgets[i]->UnderMouse({ev.pos_x, ev.pos_y})) {
-                    focus = i;
-                    break;;
-                }
-                if (i == widgets.size() - 1) {
-                    focus = -1;
-                }
-            }
-        }
-
-        if (focus != -1) {
-            widgets[focus]->Interact(ev);
-        }
-
-        for (Widget* widget : widgets) {
-            widget->Draw();
-        }
-        gout << refresh;
-    }
 }
 
 void ClearWindow() {
